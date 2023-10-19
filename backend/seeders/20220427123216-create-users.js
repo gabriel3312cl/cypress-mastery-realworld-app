6a40d7a0a16ae747890f4977d32c5476bf56c206
Hubert Arciszewski <hubertarciszewski@gmail.com>
2023-10-19 18:24:30 +0200

    Update user seed

--- backend/seeders/20220427123216-create-users.js
+++ backend/seeders/20220427123216-create-users.js
@@ -26,6 +26,20 @@ module.exports = {
         updatedAt: "2023-01-22 10:34:24.404+01",
       },
       {
+        username: "Cypress admin user",
+        email: "cypressAdminUser@example.com",
+        password: await bcryptHash("s3cret"),
+        createdAt: "2023-01-22 10:34:24.404+01",
+        updatedAt: "2023-01-22 10:34:24.404+01",
+      },
+      {
+        username: "Cypress pro user",
+        email: "cypressProUser@example.com",
+        password: await bcryptHash("s3cret"),
+        createdAt: "2023-01-22 10:34:24.404+01",
+        updatedAt: "2023-01-22 10:34:24.404+01",
+      },
+      {
         username: "Cypress user",
         email: "cypressUser@example.com",
         password: await bcryptHash("s3cret"),
